2427aaffbf64ebf2e6bfbe79dadbe6b6004c7ff6
SionPark <sopark126@github.com>
2019-03-15 15:30:15 -0700

    TSP algorithm working

--- nearestNeighbor-TSP/tsp.py
+++ nearestNeighbor-TSP/tsp.py
@@ -1,6 +1,5 @@
 
-
-file = open("cities.txt","r")
+file = open("cities_cp.txt","r")
 lines = file.read().split("\n")
 
 neighbor =""
@@ -11,106 +10,52 @@ cities = dict()
 countCities = 0
 totalWeight =0
 
+def nearestNeighbor(city):
 
-def nearest(city):
-
-    route= cities[city]
-    #print(city)
-    minimum = 10000000000000
+    route = cities[city]
+    #print(route)
+    shortestDistance = 10000000000000
     for neighbor, weight in route.items():
         weight = int(weight)
-        #print(neighbor)
-        if weight < minimum and neighbor not in visited: #compare distance, if new dist is smaller enter if
+
+        if weight < shortestDistance and neighbor not in visited: #compare distance, if new dist is smaller enter if
             city = neighbor
-            minimum = weight
-            # print("ATLANA CLOSEST")
-            # print(city)
-    return minimum, city
+            shortestDistance = weight
 
-#
-# for line in lines:
-#     line =line.split()
-#
-#     neighbor = line[1]
-#     #print(neighbor)
-#     if not visited:
-#         visited.append(neighbor) #add visited city
-#         print(visited)
-#     countCities += 1
-#
-#     if line[0] not in visited:
-#         if line[2] != 0:
-#             neighbors[line[0]] = line[2]
-#            # print(line[2])
-#
-#     if countCities < 29:
-#         #print(neighbors)
-#         currentWeight, currentNeighbor = nearest(neighbors)
-#         file =
-#         print(currentNeighbor)
-#         print (currentWeight)
-#
-#         visited.append(currentNeighbor)
-#         print(visited)
-#         totalWeight = totalWeight+ currentWeight
-#         countCities = 0
-#         neighbors.clear()
+    print(shortestDistance)
+    return shortestDistance, city
 
-for line in lines:
-    line =line.split()
-    if countCities ==0 or countCities%29 ==0:
-        neighbor = line[1]
-        print(neighbor)
-        cities[neighbor]= dict()
-        cities[neighbor][line[0]] = line[2]
-        if (countCities ==29):
+def map(lines):
+    global countCities
+    for line in lines:
+        line =line.split()
+        if countCities ==0 or countCities%30 ==0:
+            neighbor = line[0]
+            cities[neighbor]= dict()
+            cities[neighbor][line[1]] = line[2]
             countCities =0
-    else:
-        #print(neighbors)
-        cities[neighbor][line[0]] = line[2]
+        else:
+            cities[neighbor][line[1]] = line[2]
+        countCities += 1
+        # if neighbor in
+    return cities
 
-        # currentWeight, currentNeighbor = nearest(neighbors)
-        # file =
-    countCities += 1
-print(cities)
-        # print(currentNeighbor)
-        # print (currentWeight)
-        # visited.append(currentNeighbor)
-        # print(visited)
-        # totalWeight = totalWeight+ currentWeight
-        # countCities = 0
-        # neighbors.clear()
-
-def closest(city):
+def tsp(city):
     global totalWeight
-
-    visited.append(city)
-    # print(visited)
     if len(visited) ==30:
+        lastDistance = int(cities[visited[29]][visited[0]])
+        totalWeight += lastDistance
+        visited.append(visited[0])
         return visited, totalWeight
     else:
-        print("entereed")
-        minimum, closestCity = nearest(city)
-        # print(minimum)
-        # print(closestCity)
-        totalWeight += minimum
-        closest(closestCity)
-        # print(visited)
-        # print(totalWeight)
+        minimumDistance, closestCity = nearestNeighbor(city)
+        visited.append(closestCity)
+        totalWeight += minimumDistance
+        tsp(closestCity)
 
-
-
-
-closest("GulfShores")
+#put cities into a map
+map(lines)
+print(cities)
+#starting city
+tsp("SanFrancisco")
 print (visited, totalWeight)
-    # neighbor = line[1]
-    #print(neighbor)
-    # if not visited:
-    #     visited.append(neighbor) #add visited city
-    #     print(visited)
-
-
-    # if line[0] not in visited:
-    #     if line[2] != 0:
-    #         neighbors[line[0]] = line[2]
-           # print(line[2])
